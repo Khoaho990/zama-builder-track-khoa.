65377e57177b71293ac8bd6775ab54b9d2a2367f
Khoaho990 <142881276+Khoaho990@users.noreply.github.com>
2025-09-09 18:59:51 +0700

    Update EncryptedData.sol

--- contracts/EncryptedData.sol
+++ contracts/EncryptedData.sol
@@ -1,1 +1,14 @@
+// contracts/EncryptedData.sol
+pragma solidity ^0.8.0;
 
+contract EncryptedData {
+    uint256 private encryptedValue;
+
+    function setEncryptedValue(uint256 _encryptedValue) public {
+        encryptedValue = _encryptedValue;
+    }
+
+    function getEncryptedValue() public view returns (uint256) {
+        return encryptedValue;
+    }
+}
